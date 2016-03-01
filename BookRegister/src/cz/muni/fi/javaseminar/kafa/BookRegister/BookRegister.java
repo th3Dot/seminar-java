86ae3303347bc55d3de299fc60769cdda69c9e64
Martin Kalenda <martin.kalenda@ixperta.com>
2016-03-01 19:46:30 +0100

    Changed project package.

--- BookRegister/src/cz/muni/fi/javaseminar/kafa/BookRegister/BookRegister.java
+++ BookRegister/src/cz/muni/fi/javaseminar/kafa/BookRegister/BookRegister.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bookregister;
+package cz.muni.fi.javaseminar.kafa.BookRegister;
 
 /**
  *
